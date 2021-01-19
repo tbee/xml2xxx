71c3d45e3262cf964e07aff8e266c683dce0783d
Tom Eugelink <tbee@tbee.org>
2021-01-19 12:50:15 -0800

    some thought excercises

---
+++ xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
@@ -0,0 +1,18 @@
+package org.tbee.xml2yaml;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * https://yaml.org/spec/1.2/spec.html
+ */
+public class XML2YAML {
+
+	public void convert(InputStream inputStream, OutputStream outputStream) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}

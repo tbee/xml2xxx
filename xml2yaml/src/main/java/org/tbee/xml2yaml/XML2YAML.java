c79530bade968564275c8a43c29e808bfab50b4a
Tom Eugelink <tbee@tbee.org>
2021-01-21 11:03:13 -0800

    refactor, get the empty lines out

--- xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
+++ xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
@@ -10,6 +10,7 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -28,61 +29,14 @@ import javax.xml.stream.events.XMLEvent;
  */
 public class XML2YAML {
 
+	// =============================================================================================================
+	// XML processing
+	
 	/**
 	 * 
 	 */
 	public void convert(InputStream inputStream, OutputStream outputStream) {
-		convert(inputStream, outputStream, new Handler() {
-			
-			@Override
-			public void startElement(Node currentNode, Node parentNode, Writer writer) throws Exception {
-			    currentNode.isItem = "_".equals(currentNode.name);
-			    
-			    // new line if previous 
-			    if (parentNode.isItem && parentNode.previousNode == null) {
-			    	// first entry does not have a new line
-			    }
-			    else {
-			    	writer.append("\n" + indent(currentNode.indent));
-			    }
-				
-		    	if (currentNode.isItem) {
-		    		writer.append("- ");
-		    	}
-		    	else {
-		    		writer.append(currentNode.key != null ? currentNode.key : currentNode.name);
-		    		writer.append(": ");
-		        	if (currentNode.id != null) {
-		        		writer.append("&" + currentNode.id);
-		        	}
-		        	if (currentNode.ref != null) {
-		        		writer.append("*" + currentNode.ref);
-		        	}
-		    	}
-			}
-			
-			@Override
-			public void content(Node currentNode, Node parentNode, String content, Writer writer) throws Exception {
-	        	if (!content.isBlank()) {
-	        		writer.append(content);
-	        	}
-			}
-			
-			@Override
-			public void endElement(Node currentNode, Node parentNode, Writer writer) {
-			}
-		});
-	}
-	
-	
-	/*
-	 * 
-	 */
-	private void convert(InputStream inputStream, OutputStream outputStream, Handler handler) {
-		// https://www.baeldung.com/java-stax
 		try {
-			int indent = 0;
-			
 			// Prepare a stack to keep track of what happened around our current node
 			Stack<Node> stack = new Stack<>();
 			stack.push(new Node());
@@ -96,43 +50,37 @@ public class XML2YAML {
 			while (reader.hasNext()) {
 			    XMLEvent nextEvent = reader.nextEvent();
 			    			
-			    // start
+			    // setup a new node
 			    if (nextEvent.isStartElement()) {				    
 				    Node parentNode = stack.peek();
 				    Node currentNode = new Node();
 				    
 			        StartElement startElement = nextEvent.asStartElement();
 			        currentNode.name = startElement.getName().getLocalPart();
-					currentNode.key = attr(startElement, "key"); // key can overwrite name
-					currentNode.id = attr(startElement, "id"); // id can be referenced by ref
-					currentNode.ref = attr(startElement, "ref"); // ref can reference an id
-					currentNode.indent = indent;
+			        currentNode.isXml2Yaml = "xml2yaml".equals(currentNode.name);
+				    currentNode.isItem = "_".equals(currentNode.name);
+					currentNode.indent = stack.size() - 2;
 					
-				    if (!"xml2yaml".equals(currentNode.name)) {
-					    handler.startElement(currentNode, parentNode, writer);				    	
-			        	indent++;
+				    if (!currentNode.isXml2Yaml) {
+					    startElement(startElement, currentNode, parentNode, writer);				    	
 				    }
 			    	stack.push(currentNode);
 			    }
 			    
+			    // write content
 		        if (nextEvent.isCharacters()) {
-				    Node parentNode = stack.peek();
 				    Node currentNode = stack.peek();
 		        	String content = nextEvent.asCharacters().getData();
 		        	if (!content.isBlank()) {
-		        		handler.content(currentNode, parentNode, content, writer);
+	            		writer.append(content);
 		        	}
 		        }
 		        
+		        // clean up at the end of a taf
 		        if (nextEvent.isEndElement()) {
+			        EndElement endElement = nextEvent.asEndElement();
 				    Node currentNode = stack.pop();
 				    Node parentNode = stack.peek();
-
-				    if (!"xml2yaml".equals(currentNode.name)) {
-					    handler.endElement(currentNode, parentNode, writer);
-			        	indent--;
-				    }
-				    
 				    parentNode.previousNode = currentNode;
 		        }
 			}
@@ -142,31 +90,73 @@ public class XML2YAML {
 			throw new RuntimeException(e);
 		}
 	}
-	interface Handler {
-		void startElement(Node currentNode, Node parentNode, Writer writer) throws Exception;
-		void content(Node currentNode, Node parentNode, String content, Writer writer) throws Exception;
-		void endElement(Node currentNode, Node parentNode, Writer writer) throws Exception;
-	}
-	class Node {
+	private class Node {
 		String name;
 		boolean isItem = false;
-		boolean hasContent = false;
+		boolean isXml2Yaml = false;
 		Node previousNode = null;
-		String key = null;
-		String id = null;
-		String ref = null;
 		int indent;
 	}
 
+	/*
+	 * 
+	 */
+	public void startElement(StartElement startElement, Node currentNode, Node parentNode, Writer writer) throws Exception {
+		String key = attr(startElement, "key"); // key can overwrite name as the yaml key
+		if (key == null) {
+			key = currentNode.name; // but apparently it did not
+		}
+		String id = attr(startElement, "id"); // id can be referenced by ref
+		String ref = attr(startElement, "ref"); // ref can reference an id
+	    
+    	// first tag after an item does not have a new line
+	    if (parentNode.isItem && parentNode.previousNode == null) {
+	    }
+	    else {
+	    	if (!firstPrint) {
+		    	writer.append("\n");		    	
+	    	}
+	    	firstPrint = false;
+	    	writer.append(indent(currentNode.indent));
+	    }
+		
+    	if (currentNode.isItem) {
+    		writer.append("- ");
+    	}
+    	else {
+    		writer.append(key);
+    		writer.append(": ");
+        	if (id != null) {
+        		writer.append("&" + id);
+        	}
+        	if (ref != null) {
+        		writer.append("*" + ref);
+        	}
+    	}
+	}
+	private boolean firstPrint = true;
+	
+	// =============================================================================================================
+	// Support
+	
+	/*
+	 * 
+	 */
 	private String attr(StartElement startElement, String name) {
     	Attribute idAttribute = startElement.getAttributeByName(new QName(name));
 		String id = (idAttribute == null ? null : idAttribute.getValue());
 		return id;
 	}
 	
+	/*
+	 * 
+	 */
 	private String indent(int n) {
 		return "  ".repeat(n);
 	}
+	
+	// =============================================================================================================
+	// Standalone
 	
 	public static void main(String[] args) {
 		

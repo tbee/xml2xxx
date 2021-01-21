c88635f74253894fb20e80c409a03a867e1f7e03
Tom Eugelink <tbee@tbee.org>
2021-01-21 11:53:07 -0800

    added command line invocation

--- xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
+++ xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
@@ -1,5 +1,8 @@
 package org.tbee.xml2yaml;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -10,9 +13,17 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * https://yaml.org/spec/1.2/spec.html
@@ -28,6 +39,8 @@ import javax.xml.stream.events.XMLEvent;
  * - multiline strings
  */
 public class XML2YAML {
+	private static final Logger logger = LoggerFactory.getLogger(XML2YAML.class);
+	
 
 	// =============================================================================================================
 	// XML processing
@@ -40,6 +53,7 @@ public class XML2YAML {
 			// Prepare a stack to keep track of what happened around our current node
 			Stack<Node> stack = new Stack<>();
 			stack.push(new Node());
+			boolean firstPrint = true;
 			
 			// the output 
 			Writer writer = new OutputStreamWriter(outputStream,"UTF-8");
@@ -60,16 +74,17 @@ public class XML2YAML {
 			        currentNode.isXml2Yaml = "xml2yaml".equals(currentNode.name);
 				    currentNode.isItem = "_".equals(currentNode.name);
 					currentNode.indent = stack.size() - 2;
+					currentNode.firstPrint = firstPrint;
 					
 				    if (!currentNode.isXml2Yaml) {
-					    startElement(startElement, currentNode, parentNode, writer);				    	
+					    startElement(startElement, currentNode, parentNode, writer);
+					    firstPrint = currentNode.firstPrint;
 				    }
 			    	stack.push(currentNode);
 			    }
 			    
 			    // write content
 		        if (nextEvent.isCharacters()) {
-				    Node currentNode = stack.peek();
 		        	String content = nextEvent.asCharacters().getData();
 		        	if (!content.isBlank()) {
 	            		writer.append(content);
@@ -78,7 +93,6 @@ public class XML2YAML {
 		        
 		        // clean up at the end of a taf
 		        if (nextEvent.isEndElement()) {
-			        EndElement endElement = nextEvent.asEndElement();
 				    Node currentNode = stack.pop();
 				    Node parentNode = stack.peek();
 				    parentNode.previousNode = currentNode;
@@ -96,6 +110,7 @@ public class XML2YAML {
 		boolean isXml2Yaml = false;
 		Node previousNode = null;
 		int indent;
+		boolean firstPrint = true;
 	}
 
 	/*
@@ -113,10 +128,10 @@ public class XML2YAML {
 	    if (parentNode.isItem && parentNode.previousNode == null) {
 	    }
 	    else {
-	    	if (!firstPrint) {
+	    	if (!currentNode.firstPrint) {
 		    	writer.append("\n");		    	
 	    	}
-	    	firstPrint = false;
+	    	currentNode.firstPrint = false;
 	    	writer.append(indent(currentNode.indent));
 	    }
 		
@@ -134,7 +149,6 @@ public class XML2YAML {
         	}
     	}
 	}
-	private boolean firstPrint = true;
 	
 	// =============================================================================================================
 	// Support
@@ -160,5 +174,62 @@ public class XML2YAML {
 	
 	public static void main(String[] args) {
 		
+		try {
+			// create the command line parser
+			CommandLineParser parser = new DefaultParser();
+	
+			// create the Options
+			Options options = new Options();
+			options.addOption( Option.builder("h")
+					.longOpt("help")
+			        .desc("show help")
+			        .build());
+			options.addOption( Option.builder("o")
+					.longOpt("out")
+			        .desc("file to write to")
+			        .hasArg()
+			        .argName("FILE")
+			        .build());
+
+		    // parse the command line arguments
+		    CommandLine commandLine = parser.parse( options, args );
+		    if (commandLine.hasOption("help")) {
+		        new HelpFormatter().printHelp("java -jar xml2yaml.jar [OPTIONS] [FILE]", options);
+		        System.exit(1);
+		    }
+		    String[] remainingArgs = commandLine.getArgs();		    
+		    
+		    // determine input
+		    InputStream inputStream = System.in;
+		    if (remainingArgs.length == 1) {
+			    File file = new File(remainingArgs[0]);
+			    if (!file.exists()) {
+			    	System.err.print("File does not exist: "  + file.getAbsolutePath());
+			    	System.exit(2);
+			    }
+			    logger.debug("Input: " + file.getAbsolutePath());
+			    inputStream = new FileInputStream(file);
+		    }
+		    else {
+			    logger.debug("Input: STDIN");
+		    }
+		    
+		    // determine output
+		    OutputStream outputStream = System.out;
+		    if (commandLine.hasOption("out")) {
+			    File file = new File(commandLine.getOptionValue("out"));
+			    outputStream = new FileOutputStream(file);
+			    logger.debug("Output: " + file.getAbsolutePath());
+		    }
+		    else {
+			    logger.debug("Input: STDOUT");
+		    }
+		    
+		    // and convert
+		    new XML2YAML().convert(inputStream, outputStream);
+		}
+		catch(Exception exp) {
+		    System.out.println( "Unexpected exception:" + exp.getMessage() );
+		}		
 	}
 }

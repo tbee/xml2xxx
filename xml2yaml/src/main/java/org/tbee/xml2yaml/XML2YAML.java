abc356fd1d767af59d8e913d7d8d62d7167597c3
Tom Eugelink <tbee@tbee.org>
2021-01-22 10:15:57 -0800

    removed application part from core

--- xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
+++ xml2yaml/src/main/java/org/tbee/xml2yaml/XML2YAML.java
@@ -1,8 +1,5 @@
 package org.tbee.xml2yaml;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -18,12 +15,6 @@ import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -260,69 +251,5 @@ public class XML2YAML {
 	 */
 	private String indent(int n) {
 		return "  ".repeat(n);
-	}
-	
-	// =============================================================================================================
-	// Standalone
-	
-	public static void main(String[] args) {
-		
-		try {
-			// create the command line parser
-			CommandLineParser parser = new DefaultParser();
-	
-			// create the Options
-			Options options = new Options();
-			options.addOption( Option.builder("h")
-					.longOpt("help")
-			        .desc("show help")
-			        .build());
-			options.addOption( Option.builder("o")
-					.longOpt("out")
-			        .desc("file to write to")
-			        .hasArg()
-			        .argName("FILE")
-			        .build());
-
-		    // parse the command line arguments
-		    CommandLine commandLine = parser.parse( options, args );
-		    if (commandLine.hasOption("help")) {
-		        new HelpFormatter().printHelp("java -jar xml2yaml.jar [OPTIONS] [FILE]", options);
-		        System.exit(1);
-		    }
-		    String[] remainingArgs = commandLine.getArgs();		    
-		    
-		    // determine input
-		    InputStream inputStream = System.in;
-		    if (remainingArgs.length == 1) {
-			    File file = new File(remainingArgs[0]);
-			    if (!file.exists()) {
-			    	System.err.print("File does not exist: "  + file.getAbsolutePath());
-			    	System.exit(2);
-			    }
-			    logger.debug("Input: " + file.getAbsolutePath());
-			    inputStream = new FileInputStream(file);
-		    }
-		    else {
-			    logger.debug("Input: STDIN");
-		    }
-		    
-		    // determine output
-		    OutputStream outputStream = System.out;
-		    if (commandLine.hasOption("out")) {
-			    File file = new File(commandLine.getOptionValue("out"));
-			    outputStream = new FileOutputStream(file);
-			    logger.debug("Output: " + file.getAbsolutePath());
-		    }
-		    else {
-			    logger.debug("Input: STDOUT");
-		    }
-		    
-		    // and convert
-		    new XML2YAML().convert(inputStream, outputStream);
-		}
-		catch(Exception exp) {
-		    System.out.println( "Unexpected exception:" + exp.getMessage() );
-		}		
 	}
 }
